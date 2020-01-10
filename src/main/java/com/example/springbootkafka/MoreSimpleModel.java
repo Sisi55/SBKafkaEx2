97426591d351a59133e43c4179c6a365e131baf8
Sisi <dqgsh1055@gmail.com>
2020-01-11 03:27:23 +0900

    springboot + kafka: simple producer, consumer

---
+++ src/main/java/com/example/springbootkafka/MoreSimpleModel.java
@@ -0,0 +1,14 @@
+package com.example.springbootkafka;
+
+import lombok.*;
+
+@ToString
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MoreSimpleModel {
+
+    private String title;
+    private String description;
+}
---
+++ src/main/java/com/example/springbootkafka/SimpleModel.java
@@ -0,0 +1,15 @@
+package com.example.springbootkafka;
+
+import jdk.jfr.Timestamp;
+import lombok.*;
+
+@ToString
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SimpleModel {
+
+    private String field1;
+    private String field2;
+}
